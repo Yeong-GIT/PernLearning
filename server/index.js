d8853367c2359096d49dc117d86e5df870df9796
Ashisuto04 -Haoliang <ashisutotech04@gmail.com>
2023-06-05 18:53:09 +0800

    Add returning statement in pool query, and returning rows only in Postmen

--- server/index.js
+++ server/index.js
@@ -14,11 +14,11 @@ app.post("/todos", async(req, res) =>{
     //async is to wait, await
     try{
         const {description} = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", 
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", 
         [description]
         );
 
-        res.json(newTodo2)
+        res.json(newTodo.rows[0])
     }catch (err) {
         console.error(err.message);
     }
